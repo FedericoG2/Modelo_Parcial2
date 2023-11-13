a7d450a94c4b3ca14cd202eee2913dd183981755
FedericoG2 <fg0170700@gmail.com>
2023-11-13 10:43:14 -0300

    Agregar archivos de proyecto.

---
+++ modelo2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace modelo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.optCredito = new System.Windows.Forms.RadioButton();
+            this.optContado = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.cmbDispositivo = new System.Windows.Forms.ComboBox();
+            this.cmbReparación = new System.Windows.Forms.ComboBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dispositivo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Reparación";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.optCredito);
+            this.groupBox1.Controls.Add(this.optContado);
+            this.groupBox1.Location = new System.Drawing.Point(30, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 75);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Forma de Pago";
+            // 
+            // optCredito
+            // 
+            this.optCredito.AutoSize = true;
+            this.optCredito.Location = new System.Drawing.Point(154, 31);
+            this.optCredito.Name = "optCredito";
+            this.optCredito.Size = new System.Drawing.Size(58, 17);
+            this.optCredito.TabIndex = 1;
+            this.optCredito.TabStop = true;
+            this.optCredito.Text = "Crédito";
+            this.optCredito.UseVisualStyleBackColor = true;
+            // 
+            // optContado
+            // 
+            this.optContado.AutoSize = true;
+            this.optContado.Location = new System.Drawing.Point(34, 31);
+            this.optContado.Name = "optContado";
+            this.optContado.Size = new System.Drawing.Size(65, 17);
+            this.optContado.TabIndex = 0;
+            this.optContado.TabStop = true;
+            this.optContado.Text = "Contado";
+            this.optContado.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Importe Total";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(135, 31);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(280, 20);
+            this.txtCliente.TabIndex = 5;
+            // 
+            // cmbDispositivo
+            // 
+            this.cmbDispositivo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDispositivo.FormattingEnabled = true;
+            this.cmbDispositivo.Location = new System.Drawing.Point(135, 69);
+            this.cmbDispositivo.Name = "cmbDispositivo";
+            this.cmbDispositivo.Size = new System.Drawing.Size(206, 21);
+            this.cmbDispositivo.TabIndex = 6;
+            // 
+            // cmbReparación
+            // 
+            this.cmbReparación.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbReparación.FormattingEnabled = true;
+            this.cmbReparación.Location = new System.Drawing.Point(135, 111);
+            this.cmbReparación.Name = "cmbReparación";
+            this.cmbReparación.Size = new System.Drawing.Size(206, 21);
+            this.cmbReparación.TabIndex = 7;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(135, 251);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(486, 28);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(89, 33);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(486, 89);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(89, 33);
+            this.btnRegistrar.TabIndex = 10;
+            this.btnRegistrar.Text = "&Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(486, 150);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(89, 33);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(486, 211);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(89, 33);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 306);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.cmbReparación);
+            this.Controls.Add(this.cmbDispositivo);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Smart Rep";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton optCredito;
+        private System.Windows.Forms.RadioButton optContado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.ComboBox cmbDispositivo;
+        private System.Windows.Forms.ComboBox cmbReparación;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ modelo2/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modelo2
+{
+    public partial class Form1 : Form
+    {
+        // Creo la estructura 
+        public struct REPARACION
+        {
+            public string Cliente;
+            public string Dispositivo;
+            public string Reparacion;
+            public int FormaPago;
+            public float Importe;
+        };
+        // Declaro el arreglo de tipo estructura
+
+        public REPARACION[] reparaciones;
+
+        //Declaro la posicion en el arreglo
+
+        public int Posicion;
+
+        //Maxima cantidad de elementos
+
+        const int MAX = 30;
+        
+        //Variables
+
+        float ImporteTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Se carga el formulario y la etiqueta cliente y total se limpian.
+            //El boton pago contado ya aparece seleccionado por default
+
+            txtCliente.Text = "";
+            txtTotal.Text = "";
+            optContado.Checked = true;
+
+            // Combos
+
+            //Limpio los items , cargo y preselecciono el primero
+            
+            cmbDispositivo.Items.Clear();
+            cmbDispositivo.Items.Add("Smartphone");
+            cmbDispositivo.Items.Add("Tablet");
+            cmbDispositivo.Items.Add("Consola de Videojuego");
+            cmbDispositivo.SelectedIndex = 0;
+            //
+            cmbReparación.Items.Clear();
+            cmbReparación.Items.Add("Pantalla");
+            cmbReparación.Items.Add("Teclado");
+            cmbReparación.Items.Add("Sistema Operativo");
+            cmbReparación.SelectedIndex = 0;
+
+            //Se carga el boton registrar y lo quiero en falso
+
+            btnRegistrar.Enabled = false;
+
+            // Crear el arreglo de 30 elementos
+            reparaciones = new REPARACION[MAX];
+           
+            // Indice en cero para empezar a cargar
+            Posicion = 0; 
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //Limpia el nombre del cliente y el total 
+
+            txtCliente.Text = "";
+            txtTotal.Text = "";
+            
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            //Cierra el formulario
+            Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Variable total va a ir cambiando su valor durante el programa
+            float total = 0;
+
+            //Variable descuento o recargo
+
+            float dto_rec = 0; 
+            
+            // Valor total segùn el dispositivo seleccionado por el usuario
+
+            //Puede ser con switch o una sucesion de if sin anidar,sueltos
+
+            switch(cmbDispositivo.SelectedIndex)
+            {
+                case 0:
+                    total = 2500;
+                    break;
+                case 1:
+                    total = 1850;
+                    break;
+                case 2:
+                    total = 3000;
+                    break;
+            }
+
+            
+            // Al valor del dispositivo le sumo el costo de la reparación
+
+            //Un nuevo total
+
+            switch(cmbReparación.SelectedIndex)
+            {
+                case 0:
+                    total = total + 2500;
+                    break;
+                case 1:
+                    total = total + 1500;
+                    break;
+                case 2:
+                    total = total + 1000;
+                    break;
+            }
+
+            // Calcular el descuento o recargo y aplicarlo en el ultimo valor de total
+
+            if(optContado.Checked)
+            {
+                // 20% de descuento
+                dto_rec = total * 20 / 100;
+
+                // Importe final
+                total = total - dto_rec;
+            }
+            // 5% de recargo
+
+            else
+            {
+                dto_rec = total * 5 / 100;
+
+                // Importe final
+                total = total + dto_rec;
+            }
+
+            // Mostrar el resultado en la etiqueta
+
+            txtTotal.Text = total.ToString();
+
+            //Reasigno total que era local a global en la variable ImporteTotal
+
+            ImporteTotal = total;
+
+
+            // Habilitar el botón Registrar
+
+            btnRegistrar.Enabled = true;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+           //Si el nombre del cliente no esta vacio 
+
+            //Guardo en el arreglo
+            //Muevo la posicion en el arreglo
+            //Pregunto si posicion es igual al maximo o mayor deshabilito el boton calcular 
+
+            if (txtCliente.Text != "")
+            {
+                reparaciones[Posicion].Cliente = txtCliente.Text;
+                reparaciones[Posicion].Dispositivo = cmbDispositivo.Text;
+                reparaciones[Posicion].Reparacion = cmbReparación.Text;
+
+                if(optContado.Checked)
+                {
+                    reparaciones[Posicion].FormaPago = 1; // contado
+                }
+                else
+                {
+                    reparaciones[Posicion].FormaPago = 2; // credito
+                }
+                //Importe total es global
+
+                reparaciones[Posicion].Importe = ImporteTotal;
+
+                //Posicion en el arreglo
+
+                Posicion++;
+
+                if(Posicion >= MAX)
+                {
+                    //Deshabilito calcular
+                    btnCalcular.Enabled = false;
+                    MessageBox.Show("Datos completos, no se pueden agregar más reparaciones");
+                }
+                
+                //Deshabilito registrar
+                btnRegistrar.Enabled = false;
+
+
+                // Limpiar el formulario , el nombre cliente y la etiqueta total
+                txtCliente.Text = "";
+                txtTotal.Text = "";
+            }
+            else {
+                //Aparece cuando se hace click en registrar sin tener el nombre o la condicion que pida
+                MessageBox.Show("Complete los datos requeridos");
+            }
+
+        }
+    }
+}
